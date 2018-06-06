7f55ed259ee3054c7098c205f172372afcace1c1
Ssali Alex <kartsali.alex@gmail.com>
2018-06-06 18:58:17 +0300

    Makes changes to db.py file

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import psycopg2
 class Datadb():
 
     def create_table(self):
-        conn=psycopg2.connect("dbname='mainttracker' user='postgres' password='mine' host='localhost' port='5432'")
+        conn=psycopg2.connect("dbname='mainttracker' username='postgres' password='mine' host='localhost' port='5432'")
         cur=conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS requestTable (id INTEGER,name TEXT,dop INTEGER, top INTEGER, item_requested_for TEXT)")
         conn.commit()
@@ -13,26 +13,42 @@ class Datadb():
         cur=conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER,name TEXT,email TEXT, password TEXT)")
         conn.commit()
-        conn.close()
+        
 
-    def insert(self,id,name,dop,top,item_requested_for):
-        conn=psycopg2.connect("dbname='mainttracker' user='postgres' password='mine' host='localhost' port='5432'")
-        cur=conn.cursor()
-        cur.execute("INSERT INTO requestTable VALUES ('{}','{}','{}','{}','{}')" .format(id,name,dop,top,item_requested_for))
-        conn.commit()
-        conn.close()
+     def __init__():
+        self.conn = psycopg2.connect("dbname='mainttracker' username='postgres' password='main' host='localhost' port='5432'")
+        self.cur = self.conn.cursor()
+    
 
-    def update(self):
-        conn=psycopg2.connect("dbname='mainttracker' user='postgres' password='mine' host='localhost' port='5432'")
-        cur=conn.cursor()
-        cur.execute("INSERT INTO requestTable VALUES(%s,%s,%s)", (id,name,dop,top,item_requested_for))
-        conn.commit()
-        conn.close()
+    def create_request(self, id, name, dop, top, item_requested_for):
+        # new_request={'id':id,'name':name,'dop':dop,'top':top,'item requested for':item_requested_for}
+        dbscript.insert(id, name, dop, top, item_requested_for)
+        # self.details.append(new_request)
+        return 'Request added'
+        
 
-    # def view(self,id,name,dop,top,item_requested_for):
-    #     conn=psycopg2.connect("dbname='mainttracker' user='postgres' password='mine' host='localhost' port='5432'")
-    #     cur=conn.cursor()
-    #     cur.execute("INSERT INTO requestTable VALUES(%s,%s,%s)", (id,name,dop,top,item_requested_for))
-    #     conn.commit()
-    #     conn.close()
+    def get_requests(self):
+        self.cur.execute("SELECT * from requests")
+        allreq = self.cur.fetchall()
+        return allreq(allreq)
+        
+    def get_request_for_one_user(self, id):
+        self.cur.execute("SELECT *FROM requestTable WHERE id = '{}' .format(id)")
+        usr_req = self.cur.fetchone()
+        return usr_req
+
+    def update_request(self, name, item_requested_for,id):
+        self.cur.execute("UPDATE requestTable set name='{}', item_requested_for='{}' WHERE id='{}'". format(name, item_requested_for))
+        self.conn.commit()
+
+class User_auth():
+    def __init__(self):
+        self.all_user = []
+        self.id = user_id()
+        user = {'id':'id','name':'name','email':'email','username':'king','password':'password','admin':True}
+        user.append(all_user)
+
+    def sign_up(self):
+        new_user = {'id':id,'name':name,'username':username, 'password':password, 'email':email} 
+        self.all_user.append(new_user)
         
